726f764937a4422a8589baf50d6d7e1a973553f5
DESKTOP-EL875AD\Sébastien <sebastien.demortain@gmail.com>
2017-11-20 22:29:02 +0100

    piczak_cv contains the cross-validation to be run asa we have access to AWS. dirty_piczak contains the benchmark original piczak network with early stopping for validation and a test set for network comparison. Both have Xavier initialization of weights which is new compared to before. Next step : run piczak_cv as it is and plot confusion matrix for dirty_piczak as it is. Then, add stratified folds on dirty_piczak and start diagnosing it.

--- dirty_piczak.py
+++ dirty_piczak.py
@@ -209,9 +209,12 @@ print('Forward pass successful!')
 
 # Batch shit
 batch_size = 1000
-max_epochs = 300
+max_epochs = 150
 
 #Folds creation : lists with all the np.array's inside
+
+#MAKE 10 GOOD FOLDS ! ! ! ! ! ! ! !
+
 data_folds=[]
 labels_folds=[]
 for i in range(1,11):
@@ -237,87 +240,95 @@ valid_accuracy_cv = np.zeros((n_fold,max_epochs))
 with tf.Session() as sess:
     #Cross-validation
     try:
-        #Cross-validation loop
-        for k in range(n_fold):
-            print('Begin training loop',k)
-            #We reinitialize the weights
-            sess.run(tf.global_variables_initializer())
-            epoch = 0
+        #9th fold use for validation, 10th fold used for test
+        k_valid=9
+        k_test=10
+        print('Begin training loop')
+        #We reinitialize the weights
+        sess.run(tf.global_variables_initializer())
+        epoch = 0
 
-            mask=[True]*n_fold
-            mask[k]=False
-            train_data=[data_folds[i] for i in range(len(mask)) if mask[i]]
-            train_labels=[labels_folds[i] for i in range(len(mask)) if mask[i]]
-            #Merging data (list being different from np.arrays)
-            merged_train_data=np.empty((0,bands,frames,n_channels))
-            merged_train_labels=np.empty((0,num_classes))
-            for i_merge in range(n_fold-1):
-                merged_train_data=np.append(merged_train_data,data_folds[i_merge],axis=0)
-                merged_train_labels=np.append(merged_train_labels,labels_folds[i_merge],axis=0)
+        mask=[True]*n_fold
+        mask[k_valid,k_test]=False
+        train_data=[data_folds[i] for i in range(len(mask)) if mask[i]]
+        train_labels=[labels_folds[i] for i in range(len(mask)) if mask[i]]
+        #Merging data (list being different from np.arrays)
+        merged_train_data=np.empty((0,bands,frames,n_channels))
+        merged_train_labels=np.empty((0,num_classes))
+        for i_merge in range(n_fold-1):
+            merged_train_data=np.append(merged_train_data,data_folds[i_merge],axis=0)
+            merged_train_labels=np.append(merged_train_labels,labels_folds[i_merge],axis=0)
 
-            train_data=merged_train_data
-            train_labels=merged_train_labels
+        train_data=merged_train_data
+        train_labels=merged_train_labels
 
-            train_loader = bl.batch_loader(train_data, train_labels, batch_size)
+        train_loader = bl.batch_loader(train_data, train_labels, batch_size)
 
-            valid_data=data_folds[k]
-            valid_labels=labels_folds[k]
+        valid_data=data_folds[k_valid]
+        valid_labels=labels_folds[k_valid]
 
-            # Training loss and accuracy for each epoch : initialization
-            train_loss, train_accuracy = [], []
-            # Training loss and accuracy within an epoch (is erased at every new epoch)
-            _train_loss, _train_accuracy = [], []
-            valid_loss, valid_accuracy = [], []
-            test_loss, test_accuracy = [], []
+        test_data=data_folds[k_test]
+        test_labels=labels_folds[k_test]
 
-            ### TRAINING ###
-            while (epoch < max_epochs):
-                train_batch_data, train_batch_labels = train_loader.next_batch()
-                feed_dict_train = {x_pl: train_batch_data, y_pl: train_batch_labels}
-                # deciding which parts to fetch, train_op makes the classifier "train"
-                fetches_train = [train_op, cross_entropy, accuracy]
-                # running the train_op and computing the updated training loss and accuracy
-                res = sess.run(fetches=fetches_train, feed_dict=feed_dict_train)
-                # storing cross entropy (second fetch argument, so index=1)
-                _train_loss += [res[1]]
-                _train_accuracy += [res[2]]
+        # Training loss and accuracy for each epoch : initialization
+        train_loss, train_accuracy = [], []
+        # Training loss and accuracy within an epoch (is erased at every new epoch)
+        _train_loss, _train_accuracy = [], []
+        valid_loss, valid_accuracy = [], []
+        test_loss, test_accuracy = [], []
 
-                ### VALIDATING ###
-                #When we reach the last mini-batch of the epoch
-                if train_loader.is_epoch_done():
+        ### TRAINING ###
+        while (epoch < max_epochs):
+            train_batch_data, train_batch_labels = train_loader.next_batch()
+            feed_dict_train = {x_pl: train_batch_data, y_pl: train_batch_labels}
+            # deciding which parts to fetch, train_op makes the classifier "train"
+            fetches_train = [train_op, cross_entropy, accuracy]
+            # running the train_op and computing the updated training loss and accuracy
+            res = sess.run(fetches=fetches_train, feed_dict=feed_dict_train)
+            # storing cross entropy (second fetch argument, so index=1)
+            _train_loss += [res[1]]
+            _train_accuracy += [res[2]]
+
+            ### VALIDATING ###
+            #When we reach the last mini-batch of the epoch
+            if train_loader.is_epoch_done():
+                # what to feed our accuracy op
+                feed_dict_valid = {x_pl: valid_data, y_pl : valid_labels}
+                # deciding which parts to fetch
+                fetches_valid = [cross_entropy, accuracy]
+                # running the validation
+                res = sess.run(fetches=fetches_valid, feed_dict=feed_dict_valid)
+                #Update all accuracies
+                valid_loss += [res[0]]
+                valid_accuracy += [res[1]]
+                train_loss+=[np.mean(_train_loss)]
+                train_accuracy+=[np.mean(_train_accuracy)]
+                # Reinitialize the intermediate loss and accuracy within epochs
+                _train_loss, _train_accuracy = [], []
+                #Print a summary of the training and validation
+                print("Epoch {} : Train Loss {:6.3f}, Train acc {:6.3f},  Valid loss {:6.3f},  Valid acc {:6.3f}".format(
+                    epoch, train_loss[-1], train_accuracy[-1], valid_loss[-1], valid_accuracy[-1]))
+
+                #"Early stopping" (in fact, we keep going but just take the best network at every time step we have improvement)
+                if valid_accuracy[-1]==max(valid_accuracy):
+                    save_path = saver.save(sess, "./saved_models/piczak_300.ckpt",global_step=300)  # For space issues, we indicate global step being 300 but in fact it is the best_epoch step
+                    best_epoch = epoch
+                    best_valid_accuracy = valid_accuracy[-1]
+
+                    #Compute the test error (easier than restoring the file)
                     # what to feed our accuracy op
-                    feed_dict_valid = {x_pl: valid_data, y_pl : valid_labels}
+                    feed_dict_test = {x_pl: test_data, y_pl: test_labels}
                     # deciding which parts to fetch
-                    fetches_valid = [cross_entropy, accuracy]
+                    fetches_test = [cross_entropy, accuracy]
                     # running the validation
-                    res = sess.run(fetches=fetches_valid, feed_dict=feed_dict_valid)
-                    #Update all accuracies
-                    valid_loss += [res[0]]
-                    valid_accuracy += [res[1]]
-                    train_loss+=[np.mean(_train_loss)]
-                    train_accuracy+=[np.mean(_train_accuracy)]
-                    # Reinitialize the intermediate loss and accuracy within epochs
-                    _train_loss, _train_accuracy = [], []
-                    #Print a summary of the training and validation
-                    print("Epoch {} : Train Loss {:6.3f}, Train acc {:6.3f},  Valid loss {:6.3f},  Valid acc {:6.3f}".format(
-                        epoch, train_loss[-1], train_accuracy[-1], valid_loss[-1], valid_accuracy[-1]))
+                    res = sess.run(fetches=fetches_test, feed_dict=feed_dict_test)
+                    # Update all accuracies
+                    test_loss = res[0]
+                    test_accuracy = res[1]
 
-                    #Update the cross validation results
-                    train_loss_cv[k,epoch]=train_loss[-1]
-                    train_accuracy_cv[k,epoch]=train_accuracy[-1]
-                    valid_loss_cv[k,epoch]=valid_loss[-1]
-                    valid_accuracy_cv[k,epoch]=valid_accuracy[-1]
+                # Update epoch
+                epoch += 1;
 
-                    #"Early stopping" (in fact, we keep going but just take the best network at every time step we have improvement)
-                    if valid_accuracy[-1]==max(valid_accuracy):
-                        save_path = saver.save(sess, "./saved_models/piczak_300.ckpt",global_step=300)  # For space issues, we indicate global step being 300 but in fact it is the best_epoch step
-                        best_epoch=epoch
-                        best_valid_accuracy = valid_accuracy[-1]
-
-                    # Update epoch
-                    epoch += 1;
-
-                    # whatever = 1000
     except KeyboardInterrupt:
         pass
 
@@ -326,9 +337,10 @@ print("model saved under the path: ", save_path)
 
 #Saving stuff
 #mdict={'train_loss_cv':train_loss_cv,'train_accuracy_cv':train_accuracy_cv,'valid_loss_cv':valid_loss_cv,'valid_accuracy_cv':valid_accuracy_cv}
-mdict={'train_loss':train_loss,'train_accuracy':train_accuracy,'valid_loss':valid_loss,'valid_accuracy':valid_accuracy,'best_epoch':best_epoch,'best_valid_accuracy':best_valid_accuracy}
+mdict={'train_loss':train_loss,'train_accuracy':train_accuracy,'valid_loss':valid_loss,'valid_accuracy':valid_accuracy,
+       'test_loss':test_loss,'test_accuracy':test_accuracy,'best_epoch':best_epoch,'best_valid_accuracy':best_valid_accuracy}
 
-scipy.io.savemat('piczak_cv.mat',mdict)
+scipy.io.savemat('dirty_piczak.mat',mdict)
 
         #TODO: fix this
 #plt.figure()
